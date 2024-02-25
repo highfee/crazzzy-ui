b8d9cd67557a9155accd041379ea4cba6bb130c2
highfee <odediranifeoluwa7@gmail.com>
2024-02-25 11:57:05 +0100

    changed active chain to cronos

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,8 +1,34 @@
 "use client";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  trustWallet,
+  phantomWallet,
+  localWallet,
+} from "@thirdweb-dev/react";
+import { Cronos } from "@thirdweb-dev/chains";
 
 const Client = ({ children }) => {
-  return <ThirdwebProvider activeChain="mumbai">{children}</ThirdwebProvider>;
+  return (
+    <ThirdwebProvider
+      clientId="1639134fe6d77249631aa361f3a9cbe1"
+      activeChain={Cronos}
+      supportedWallets={[
+        metamaskWallet({
+          recommended: true,
+        }),
+        coinbaseWallet(),
+        walletConnect(),
+        trustWallet(),
+        phantomWallet(),
+        localWallet(),
+      ]}
+    >
+      {children}
+    </ThirdwebProvider>
+  );
 };
 
 export default Client;
