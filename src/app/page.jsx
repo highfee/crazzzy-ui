50a255aef1dd0f499acbbbda6e478a2582ec3ea0
highfee <odediranifeoluwa7@gmail.com>
2024-05-18 22:43:06 +0100

    Ife: 18th May

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,10 @@ import NFTs from "@/components/NFTs";
 import StakingInfo from "@/components/StakingInfo";
 import Image from "next/image";
 
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  redirect("/stake");
   return (
     <ContainerLayout>
       <main className="mt-20 flex flex-col-reverse lg:flex-row gap-10">
